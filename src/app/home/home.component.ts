b1c4e6f32bf176370dfff26c803cbde601b0c34c
Mike B <busbyBAM@gmail.com>
2019-04-06 09:30:04 -0400

    add counter and subtract button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Image } from "tns-core-modules/ui/image";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  title = 'tomato-time';
+  title = "Tomato Time";
+  count = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  tomatoTapped() {
+    this.count++;
+  }
+
+  cancelTapped() {
+    if (this.count > 0) {
+      this.count--;
+    }
   }
 }
